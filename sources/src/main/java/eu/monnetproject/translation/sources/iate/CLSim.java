021bb0c57e75bc2838ea875df022456f001ca585
Kartik Asooja <asooja@gmail.com>
2013-01-29 22:39:07 +0000

    git-svn-id: https://subversion.deri.ie/monnet_dev/sources/trunk/translation@4372 18474ff0-dd96-4de7-8b2b-b9d0bd1f8700

--- sources/src/main/java/eu/monnetproject/translation/sources/iate/CLSim.java
+++
@@ -1,7 +0,0 @@
-package eu.monnetproject.translation.sources.iate;
-
-import eu.monnetproject.lang.Language;
-
-public interface CLSim {
-	public double score(String text1, Language lang1, String text2, Language lang2);	
-}
