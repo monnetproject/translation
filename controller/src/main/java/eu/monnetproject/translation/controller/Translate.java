cece9308caf3c34254a59b364a19fcabe4aa9324
John McCrae <john@mccr.ae>
2013-03-08 18:15:21 +0100

    Add translate script

--- controller/src/main/java/eu/monnetproject/translation/controller/Translate.java
+++ controller/src/main/java/eu/monnetproject/translation/controller/Translate.java
@@ -27,20 +27,22 @@
 package eu.monnetproject.translation.controller;
 
 import eu.monnetproject.lang.Language;
-import eu.monnetproject.lemon.model.LexicalEntry;
-import eu.monnetproject.lemon.model.LexicalSense;
 import eu.monnetproject.lemon.model.Lexicon;
 import eu.monnetproject.ontology.Ontology;
 import eu.monnetproject.ontology.OntologySerializer;
 import eu.monnetproject.framework.services.Services;
 import eu.monnetproject.label.LabelExtractorFactory;
+import eu.monnetproject.lemon.LemonSerializer;
 import eu.monnetproject.translation.*;
 import eu.monnetproject.translation.controller.impl.SimpleLexicalizer;
 import eu.monnetproject.translation.controller.impl.TranslationController;
 import java.io.File;
 import java.io.FileReader;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.io.Reader;
+import java.io.Writer;
 import java.net.URI;
 import java.net.URL;
 import java.util.ArrayList;
@@ -56,14 +58,12 @@ import java.util.List;
 public class Translate {
 
     public static void main(String[] args) throws Exception {
-     //   if(args.length == 0) {
-     //       args = "tmp/ontology.rdf es".split(" ");
-     //   }
         final List<String> argsList = new LinkedList<String>(Arrays.asList(args));
         String namePrefix = null;
         int nBest = 1;
         String[] scopeStrs = new String[]{};
         boolean verbose = false;
+        boolean fast = false;
         for (int i = 0; i < argsList.size(); i++) {
             if (argsList.get(i).equals("-namePrefix")) {
                 namePrefix = argsList.get(i + 1);
@@ -83,14 +83,18 @@ public class Translate {
                 argsList.remove(i);
                 argsList.remove(i);
                 i--;
+            } else if(argsList.get(i).equals("-fast")) {
+                fast = true;
+                argsList.remove(i);
             } else if (argsList.get(i).equals("-verbose")) {
                 verbose = true;
                 argsList.remove(i);
             }
         }
         final OntologySerializer ontoSerializer = Services.get(OntologySerializer.class);
-        if (argsList.size() != 2) {
-            System.err.println("Usage:\n\ttranslate [-namePrefix prefix] [-nBest n] [-scope uri1,uri2,uri3...] ontologyFileOrUrl targetLanguage");
+        final LemonSerializer lemonSerializer = Services.get(LemonSerializer.class);
+        if (argsList.size() != 2 && argsList.size() != 3) {
+            System.err.println("Usage:\n\ttranslate [-namePrefix prefix] [-nBest n] [-scope uri1,uri2,uri3...] [-fast] ontologyFileOrUrl targetLanguage [ontologyOut]");
             System.exit(-1);
         }
         final String ontologyFileOrUrl = argsList.get(0);
@@ -106,6 +110,9 @@ public class Translate {
             }
             ontologyReader = new FileReader(ontologyFileOrUrl);
         }
+        final Writer out = argsList.size() == 3 ?
+                new PrintWriter(argsList.get(2)) :
+                new OutputStreamWriter(System.out);
         final Ontology ontology = ontoSerializer.read(ontologyReader);
 
         final TranslationController controller = new TranslationController(Services.getAll(LanguageModelFactory.class),
@@ -137,13 +144,10 @@ public class Translate {
 
         controller.setVerbose(verbose);
 
-        controller.translate(ontology, sourceLexica, targetLexicon, scopes, namePrefix, nBest);
+        controller.translate(ontology, sourceLexica, targetLexicon, scopes, namePrefix, nBest, fast ? OntologyTranslator.DECODE_FAST : 0);
 
-        for (LexicalEntry entry : targetLexicon.getEntrys()) {
-            for (LexicalSense sense : entry.getSenses()) {
-                System.out.println("<"+sense.getReference() + "> rdfs:label " + entry.getCanonicalForm().getWrittenRep());
-            }
-        }
-        System.exit(0);
+        lemonSerializer.write(targetLexicon.getModel(), out);
+        out.flush();
+        out.close();
     }
 }
